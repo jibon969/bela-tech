25b25b3ea8b95d092a9f5a508a014f3cd0e29b32
= <help.studentgrade@gmail.com>
2022-09-02 21:59:18 +0600

    added bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
-
 import Head from "next/head";
-import Script from "next/script";
+import Script from "next/script"
+import '../styles/globals.css'
 
 import Footer from '../layouts/Footer/Footer'
 
@@ -9,21 +8,22 @@ function MyApp({Component, pageProps}) {
     return (
         <>
             <Head>
-                <meta charSet="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                    rel="stylesheet"
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                    crossorigin="anonymous"
+                />
             </Head>
 
             <Script
-                src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-                crossOrigin="anonymous"
-            />
-
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+                crossorigin="anonymous"/>
 
             <Component {...pageProps} />
-
             <Footer/>
-
         </>
     );
 }
