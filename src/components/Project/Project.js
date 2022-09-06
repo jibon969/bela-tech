43d181f1c3db71cc68458325a648523160370b64
= <help.studentgrade@gmail.com>
2022-09-06 12:01:08 +0600

    added project

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,4 +1,4 @@
-import styles from './Team.module.css'
+import styles from './Project.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 import team from '../../../src/assets/team/team.jpg'
@@ -7,35 +7,145 @@ import team2 from '../../../src/assets/team/team2.jpg'
 import team3 from '../../../src/assets/team/team2.jpg'
 import team4 from '../../../src/assets/team/team3.jpg'
 
-const Team = () => {
+const Project = () => {
     return (
         <div>
-            <div className={`${styles.team}`}>
+            <section className={styles.projectSection}>
                 <div className="container">
-                    <h2 className={`${styles.teamTitle} mb-3`}>Our Team</h2>
-                    <p className="text-center mb-5">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
-                    </p>
-
+                    <div className={styles.projectTitle}>
+                        <h2>Check Some Of Our Recent Work</h2>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
                     <div className="row">
-                        <div className="col-md-6 col-lg-4">
-                            <div className="card border-0 transform-on-hover">
-                                <a className="lightbox" href={{team}}>
-                                    <Image src={{team}} alt="Card Image" className="card-img-top"/>
-                                </a>
-                                <div className="card-body">
-                                    <h6><a href="#">Lorem Ipsum</a></h6>
-                                    <p className="text-muted card-text">Lorem ipsum dolor sit amet, consectetur
-                                        adipiscing elit. Nunc quam urna.</p>
-                                </div>
-                            </div>
+                        <div className="col">
+                            <ul className={`${styles.image_gallery} list-unstyled`}>
+                                <li>
+                                    <img src="https://source.unsplash.com/DqyYTM7pR2o/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="https://source.unsplash.com/e6FMMambeO4/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="https://source.unsplash.com/klCiPmzUw0Y/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="https://source.unsplash.com/IdNOTjPeHrE/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="https://source.unsplash.com/O0N9MF--hK4/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="https://source.unsplash.com/FV3GConVSss/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="https://source.unsplash.com/0ESjL-Nw22Y/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="https://source.unsplash.com/KTVn62x6fFw/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="https://source.unsplash.com/VSeVhmW4_JQ/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="https://source.unsplash.com/07aFaTf24Kg/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="https://source.unsplash.com/DqyYTM7pR2o/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="https://source.unsplash.com/0ESjL-Nw22Y/640x416" alt=""/>
+                                    <div className={styles.overlay}>
+                                        <span>
+                                            <Link href="">
+                                                <a className={styles.clickHere}>Click Here</a>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
         </div>
-        < /div>
     )
 };
 
-export default Team;
+export default Project;
